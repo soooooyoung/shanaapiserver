46fc0d004db8a7df6c5ae45c098e67218a67a1a6
SooyoungKang <kang.sooyoung@hotmail.com>
2024-06-27 19:07:48 +0900

    Change rate limit to 50 requests

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { logError, logInfo } from "./utils/Logger";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 10,
+  limit: 50,
   standardHeaders: "draft-7",
   legacyHeaders: false,
 });
